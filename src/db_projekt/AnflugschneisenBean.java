3e963c11e976f652e5db007a8b0eef72085bcbdf
Richard <richard@frenzel.net>
2018-01-11 13:29:59 +0100

    idk yet

---
+++ src/db_projekt/AnflugschneisenBean.java
@@ -0,0 +1,13 @@
+package db_projekt;
+
+public class AnflugschneisenBean {
+	private int schneisennummer;
+
+	public int getSchneisennummer() {
+		return schneisennummer;
+	}
+
+	public void setSchneisennummer(int schneisennummer) {
+		this.schneisennummer = schneisennummer;
+	}
+}
